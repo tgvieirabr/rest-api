802eac0f153c0652de0c23d5a164b158b872264f
tgvieirabr <tgvieirabr@gmail.com>
2023-08-25 17:31:12 -0300

    #

---
+++ apitgdev/app/public/wp-content/themes/api/endpoints/user_post.php
@@ -0,0 +1,22 @@
+<?php 
+
+function api_user_post ($request) {
+
+$response = [
+'ID' => '2',
+'user_login' => 'meu_usuario',
+
+];
+
+return rest_ensure_response();
+
+}
+
+function register_api_user_post() {
+
+
+    
+}
+
+?>
+
