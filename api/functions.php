3b3d75f242fa698254e208e11a2af69b5a98f805
Tiago Vieira <tgvieirabr@gmail.com>
2023-08-28 11:49:14 -0300

    Add files via upload

---
+++ api/functions.php
@@ -0,0 +1,14 @@
+<?php 
+
+remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+$dirbase = get_template_directory();
+require_once $dirbase . '/endpoints/user_post.php';
+
+
+function change_api() {
+    return 'json';
+}
+add_filter('rest_url_prefix', 'change_api'); 
+
+?>
